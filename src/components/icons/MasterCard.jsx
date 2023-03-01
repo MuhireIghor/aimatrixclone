d32a702bd2b97ecdc9d52b279275c3f30cd48a92
MuhireIghor <muhireighor123@gmail.com>
2023-03-01 17:40:20 +0200

    [feat]:Mastercard svg component

---
+++ src/components/icons/MasterCard.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+
+const MasterCard = ({width,height}) => {
+  return (
+    
+<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width={width} height={height}
+	 viewBox="0 0 512 512" style={{enableBackground:'new 0 0 512 512'}} xmlSpace="preserve" >
+
+<g>
+	<path fill='#EA001B' clipRule={"evenodd"} fillRule={"evenodd"} d="M149.3,106.7c82.5,0,149.3,66.9,149.3,149.3s-66.9,149.3-149.3,149.3S0,338.5,0,256S66.9,106.7,149.3,106.7"/>
+	<path fill='#FFB533' d="M362.7,106.7c-41.8,0-79.6,17.2-106.7,44.8c26.4,26.9,42.7,63.8,42.7,104.5s-16.3,77.6-42.7,104.5
+		c27.1,27.7,64.9,44.8,106.7,44.8c82.5,0,149.3-66.9,149.3-149.3S445.1,106.7,362.7,106.7"/>
+	<path fill='#FB8205' d="M256,151.5c-26.4,26.9-42.7,63.8-42.7,104.5s16.3,77.6,42.7,104.5c26.4-26.9,42.7-63.8,42.7-104.5
+		S282.4,178.4,256,151.5"/>
+</g>
+</svg>
+
+  )
+}
+
+export default MasterCard
