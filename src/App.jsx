2369494edc28b55b3aac82b2ce725fe1221d26d1
MuhireIghor <muhireighor123@gmail.com>
2023-03-01 17:54:12 +0200

    app structure and utilities

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import AppProvider from './provider/AppProvider'
+import Routes from './Routes'
+
+function App() {
+
+  return (
+<AppProvider>
+  <Routes/>
+</AppProvider>
+  )
+}
+
+export default App
