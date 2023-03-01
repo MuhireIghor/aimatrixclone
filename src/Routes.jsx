581c8866e9a05869c30d040cb9d917898ea69fd9
MuhireIghor <muhireighor123@gmail.com>
2023-03-01 17:53:03 +0200

    [feat]:App routing

---
+++ src/Routes.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import {useRoutes} from 'react-router-dom'
+import Aboutus from './components/About/Aboutus';
+import Contacts from './components/contacts/Contacts';
+import Maincomponent from './components/Maincomponent';
+import Notfoundpage from './components/Notfoundpage';
+import Homelayout from './layouts/Homelayout';
+
+const Routes = () => {
+  return useRoutes([{
+    path:"/",
+    element:<Homelayout />,
+    children:[
+        {
+            index:true,element:<Maincomponent/> 
+        },{
+
+          path:"/pages/contact",
+          element:<Contacts />
+        },{
+          path:"/pages/discover-the-power-of-matrix-ai-movement-our-story-and-mission-to-empower-you-with-training-tools-and-e-commerce-store",
+          element:<Aboutus />
+        }
+    ]
+  }
+  ,{
+    path:"*",
+    element:<Notfoundpage />
+  }])
+}
+
+export default Routes
