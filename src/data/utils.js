0392a0f8d2ba8fed73ac81aa6b70784ebed3f4d5
MuhireIghor <muhireighor123@gmail.com>
2023-03-01 17:52:22 +0200

    [feat]:utility data store

---
+++ src/data/utils.js
@@ -0,0 +1,60 @@
+export const links = [
+    {
+        name: "Home",
+        reference: ""
+    },
+    {
+        name: "Store",
+        reference: "collections/all"
+    },
+    {
+        name: "Contact",
+        reference: "pages/contact"
+    },
+    {
+        name: "About Us",
+        reference: "pages/discover-the-power-of-matrix-ai-movement-our-story-and-mission-to-empower-you-with-training-tools-and-e-commerce-store"
+    },
+]
+
+export const steps = [
+    {
+        name: "Step 1",
+        description: " Access our e-commerce store for powerful courses on psychology, crypto, IT, and more, designed to help you develop skills and knowledge to thrive in the modern digital world."
+    },
+    {
+        name: "Step 2",
+        description: " Join our agency to gain access to exclusive job opportunities, training, and powerful tools that can help you start or grow your career and business for FREE."
+    },
+    {
+        name: "Step 3",
+        description: " In order to truly escape the matrix, it's important to surround yourself with people who share your mindset and values. That's why our agency offers a community of like-minded individuals who are all working towards financial freedom and personal growth. Through our online platform, you can connect with others, share your experiences, and learn from each other. This community is a great place to find support and encouragement as you work towards your goals."
+    },
+    {
+        name: "Step 4",
+        description: " Use the skills and knowledge you've acquired to build a life of abundance, independence, and freedom. Whether you're looking to start a new business, build a side hustle, or pursue a fulfilling career, our services can help you get there faster and more efficiently than you ever thought possible."
+    }
+]
+export const aboutLinks = [
+    "About us",
+    "Refund Policy",
+    "Privacy Policy",
+    "Terms Of Service",
+    "Legal Notice",
+    "Contact Information"
+]
+export const descphrases = [
+    "Welcome to Matrix AI Movement, the e-commerce store and agency that's changing the game in more ways than one. Our mission is to empower people and businesses with the knowledge and tools they need to succeed in today's ever-evolving world. We believe in the power of technology and education to make a positive impact on people's lives.",
+    "At Matrix AI Movement, we offer a wide range of products and services to help individuals and businesses thrive. Our e-commerce store provides a wealth of valuable resources, including courses on psychology, cryptocurrency, and much more. Whether you're interested in learning new skills or looking for ways to improve your existing business, we've got you covered.",
+    "In addition to our e-commerce store, we also offer an agency service, providing individuals and businesses with the support they need to take their ventures to the next level. We offer a range of services, including digital marketing, branding, and web design, as well as training and coaching to help you build your skills and knowledge.",
+    "But that's not all. We also believe in giving back, which is why we offer free training and opportunities for individuals to make money with all the necessary tools provided. All we ask in return is a commitment and a positive mindset to succeed.",
+    "At Matrix AI Movement, we are committed to providing the highest level of quality and service to our customers. Our products are carefully curated to ensure that they meet our high standards, and our team is dedicated to helping you achieve your goals.",
+    "We take your privacy and security seriously, and we have robust measures in place to protect your data and your transactions. We offer a 7-day no-questions-asked refund policy for products that you haven't received, but we cannot offer refunds for products that have already been opened.",
+    "We also have a legal notice that prohibits the sharing of products via email with others. We take this seriously, and any violation will result in the termination of the product and further action on our part.",
+    "We believe in building strong relationships with our customers, and we're committed to providing exceptional service and support. If you have any questions or concerns, please don't hesitate to contact us. We're here to help you succeed.",
+    "Continuing from our earlier description, at Matrix AI Movement, we believe that creating a better future for ourselves and the generations to come requires collaboration, innovation, and a commitment to excellence. That's why we are dedicated to providing people with the training, tools, and support they need to succeed in their goals and dreams.",
+    "Our agency arm offers a range of services to help people grow their businesses and achieve their full potential. From marketing and branding to web design and development, we work closely with our clients to understand their needs and deliver customized solutions that meet and exceed their expectations.",
+    "Our e-commerce store provides a wide range of courses, including ones on psychology, cryptocurrency, IT, and more, to help people learn new skills and explore new opportunities. With our extensive selection of training and educational resources, you'll be able to grow your knowledge and gain valuable insights into various industries and markets.",
+    "At Matrix AI Movement, we understand that financial freedom is one of the most important goals for many people. That's why we offer training and opportunities to make money, with all the necessary tools and support to succeed. With our commitment to transparency and our seven-day no-questions-asked refund policy, you can trust that you're in good hands.",
+    "So whether you're looking to start a new business, learn new skills, or grow your existing business, we have everything you need to achieve your goals. Join the Matrix AI Movement today and let us help you reach your full potential."
+]
