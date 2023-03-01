ddcbc717bf084cbb1c2cdf65f13e283b4c28cf4a
MuhireIghor <muhireighor123@gmail.com>
2023-03-01 17:43:59 +0200

    [feat]:Error page component

---
+++ src/components/ErrorPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const ErrorPage = () => {
+  return (
+    <div className=' w-full h-screen bg-primarycolor flex justify-center items-center text-4xl tracking-widest font-semibold text-linkColor '>
+      <div className='space-y-8'>
+<p> 🤭404 OOPS SOMETHING WENT WRONG 🤭</p>
+<p className='flex justify-center text-2xl'>Internal Application Error</p>
+
+      </div>
+    </div>
+  )
+}
+
+export default ErrorPage
