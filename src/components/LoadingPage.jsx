afbe5c5a7900843d5ad8285fc0a799b4cb565dc4
MuhireIghor <muhireighor123@gmail.com>
2023-03-02 22:53:48 +0200

    [update]:Loading page rebuild

--- src/components/LoadingPage.jsx
+++ src/components/LoadingPage.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import React from 'react';
+import '../styles/Loader.css';
 
 const LoadingPage = () => {
   return (
-    <div>LoadingPage</div>
+    <div className='w-full h-screen flex bg-primarycolor items-center justify-center '>
+      <div className='space-y-12'>
+      <div className='loader'></div>
+      <p className='text-linkColor text-lg tracking-wider animate-pulse flex justify-center'>Loading</p>
+
+      </div>
+
+    </div>
   )
 }
 
